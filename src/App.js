dbf26546380093d6b78c215107de33c1329d939d
mrcomputer2018 <45561350+mrcomputer2018@users.noreply.github.com>
2021-08-11 15:54:53 -0300

    acertos

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
+import React from "react"
+import logo from './assets/logo.svg';
 import './App.css';
+import Button from "./components/button";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Button/>
         <a
           className="App-link"
           href="https://reactjs.org"
