1b499857b2568c4c79ee8e7c69e6cff78cc827ba
mrcomputer2018 <45561350+mrcomputer2018@users.noreply.github.com>
2021-08-17 16:26:24 -0300

    componente primeiro

---
+++ src/components/basicos/Primeiro.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+function Primeiro() {
+    const msg = "Seja bem vindo!!!"
+
+    return (
+        <div>
+            <h2>"Primeiro componente"</h2>
+            <p>{msg}</p>
+        </div>
+    )
+}
+
+export default Primeiro;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import ReactDOM from "react-dom"
 //* Para poder usar JSX
 import React from "react"
+import "./index.css"
+
+import Primeiro from "./components/basicos/Primeiro"
 
 const element = document.getElementById("root")
 
 ReactDOM.render(
     <div>
         <strong>"Ola, React!!!!"</strong>
+        <br/>
+        <Primeiro/>
     </div>, 
     element
     )
