1dd0c9359903aed7fc97e5cbf8abfb829437ae12
mrcomputer2018 <45561350+mrcomputer2018@users.noreply.github.com>
2021-08-17 18:49:59 -0300

    props somente leitura

--- src/components/basicos/ComParametro.jsx
+++ src/components/basicos/ComParametro.jsx
@@ -3,13 +3,15 @@ import React from "react"
 function ComParametro(props) {
     console.log(props);
     const status = props.nota >= 7 ? "Aprovado" : "Reprovado"
-
+    const notaInt = Math.ceil(props.nota)
     return (
         <div>
+            {/* As props sao somente leitura */}
             <h2>{ props.titulo }</h2>
             <h3>Aluno: { props.subtitulo }</h3>
             <p>tem nota <strong>{ props.nota }</strong></p>
-            <p> e foi <strong>{ status }</strong></p>!!!
+            <p> e foi <strong>{ status }</strong>!!!</p>
+            <p>Nota alterada para teste { notaInt }</p>
         </div>
     )
 }
