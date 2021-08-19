b711d8ab16844ea6b12cfa396ae53956e3fcb413
mrcomputer2018 <45561350+mrcomputer2018@users.noreply.github.com>
2021-08-19 11:34:02 -0300

    Card 02 layout

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import "./App.css"
 
 import Primeiro from "./components/basicos/Primeiro"
 import ComParametro from "./components/basicos/ComParametro"
@@ -8,27 +9,29 @@ import Card from "./components/layout/Card"
 
 function App(props) {
     return (
-        <div id="app">
+        <div className="app">
             <h1>Fundamentos React</h1>
             <br/>
-            <Card titulo="#01 - Primeiro">
-                <Primeiro/>
-            </Card>
-            
-            <Card titulo="#02 - Com Parametro">
-                <ComParametro 
-                    titulo="Situção do aluno:" 
-                    subtitulo="Pedro Silva" 
-                    nota={10} />
-            </Card>
-            
-            <Card titulo="#03 - Fragmento">
-                <Fragmento />
-            </Card>
+            <div className="cards">
+                <Card titulo="#01 - Primeiro"  color="#588C73">
+                    <Primeiro/>
+                </Card>
+                
+                <Card titulo="#02 - Com Parametro"  color="#E8B71A">
+                    <ComParametro 
+                        titulo="Situção do aluno:" 
+                        subtitulo="Pedro Silva" 
+                        nota={10} />
+                </Card>
+                
+                <Card titulo="#03 - Fragmento" color="#E94C6F">
+                    <Fragmento />
+                </Card>
 
-            <Card titulo="#04 - Desafio Aleatorio">
-                <Aleatorio min={1} max={60}/>
-            </Card>
+                <Card titulo="#04 - Desafio Aleatorio" color="#FA6900">
+                    <Aleatorio min={1} max={60}/>
+                </Card>
+            </div>
         </div>
     )
 }
--- src/components/layout/Card.jsx
+++ src/components/layout/Card.jsx
@@ -2,10 +2,15 @@ import React from "react"
 import "./Card.css"
 
 const Card = (props) => {
-    const { titulo, children } = props
+    const { titulo, children, color } = props
+
+    const cardStyle = {
+        backgroundColor: color || "#f00",
+        borderColor: color || "#f00"
+    }
 
     return (
-        <div className="Card">
+        <div className="Card" style={ cardStyle }>
             <div className="Tittle">{ titulo }</div>
             <div className="Content">{ children }</div>
         </div>
