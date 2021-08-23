a83a474a3465f0f3989a721f454d65f1d6a86ab0
mrcomputer2018 <45561350+mrcomputer2018@users.noreply.github.com>
2021-08-23 11:29:43 -0300

    componente com estado

--- src/components/comunicacao/IndiretaFilho.jsx
+++ src/components/comunicacao/IndiretaFilho.jsx
@@ -1,14 +1,19 @@
 import React from "react"
 
 const IndiretaFilho = (props) => {
-    
+    const min = 50
+    const max = 70
+
+    const gerarIdade = () => parseInt(Math.random() * (max - min)) + min
+    const gearNerd = () => Math.random() > 0.5 
+
     return (
         <div>
             <div>Filho</div>
             {/* Ao clicar envia informacoes pro componente pai*/}
             <button onClick={
                 function(e) {
-                    props.quandoClicar('joao', 53, true)
+                    props.quandoClicar('joao', gerarIdade(), gearNerd())
                 }}>
                 Fornecer Informacoes
             </button>
--- src/components/comunicacao/IndiretaPai.jsx
+++ src/components/comunicacao/IndiretaPai.jsx
@@ -1,13 +1,26 @@
-import React from "react"
+import React, { useState } from "react"
 import IndiretaFilho from "./IndiretaFilho";
 
 const IndiretaPai = (props) => {
+   /*  primeiro - o proprio valor que e nome */
+   /* segundo - e uma funcao que vou usar para alterar o valor */
+    let [nome, setNome] = useState('?')
+    let [idade, setIdade] = useState(0)
+    let [nerd, setNerd]  = useState(false)
+
     function fornecerInformacoes (nome, idade, nerd) {
-        console.log(nome, idade, nerd);
+       /*  alterando Valores */
+        setNome(nome)
+        setIdade(idade)
+        setNerd(nerd)
     }
 
     return (
         <div>
+            <span>{ nome } </span>
+            <span><strong>{ idade } </strong></span>
+            <span>{ nerd ? "Verdadeiro" : "Falso"}</span>
+
             <div>Pai</div>
             <IndiretaFilho quandoClicar={fornecerInformacoes}></IndiretaFilho>
         </div>
